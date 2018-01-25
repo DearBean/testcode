06576372128d4de963be0273d8eecdd03efc0f73
Usernames <Usernames@957GS62.ad.shared>
2018-01-25 12:48:48 +0700

    connect test

--- index.php
+++ index.php
@@ -78,6 +78,10 @@ $hostname = "testdb.cga2wyvjsrqq.us-east-2.rds.amazonaws.com:3306";
 echo "Start to connecet";
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password);
+if(!dbhandle){
+	die('fail'.mysql_error());
+}
+mysql_close($dbhandle);
 echo "Connected to MySQL<br>";
 
 ?>
